35a6bdabce83dcaa2f0ea63e835e888c20c9de42
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-12-02 09:57:09 -0500

    testing changes

--- functions.php
+++ functions.php
@@ -6,3 +6,9 @@ function public_nav_main_bootstrap() {
     $nav->setPartial($partial);
     return $nav->render();
 }
+function public_nav_items_bootstrap() {
+    $partial = array('common/menu-items-partial.phtml', 'default');
+    $nav = public_nav_items();  // this looks like $this->navigation()->menu() from Zend
+    $nav->setPartial($partial);
+    return $nav->render();
+}
--- items/browse.php
+++ items/browse.php
@@ -4,7 +4,7 @@
 ?>
 
     <h1><?php echo 'Browse all items'; ?></h1>
-    <?php echo public_nav_items(); ?>
+    <?php echo public_nav_items_bootstrap; ?>
     <hr>    
 
     <div class="browse-items">
@@ -13,22 +13,6 @@
             $sortLinks[__('Title')] = 'Dublin Core,Title';
             $sortLinks[__('Creator')] = 'Dublin Core,Creator';
             ?>
-			<div class="browse-items-header hidden-xs">
-                <div class="row">
-                    <div class="col-sm-3 col-sm-offset-2 col-md-2 col-md-offset-2">
-                        <?php echo browse_sort_links(array('Title'=>'Dublin Core,Title'), array('')); ?>
-                    </div>
-                    <div class="col-sm-3 col-md-2">
-                        <?php echo browse_sort_links(array('Creator'=>'Dublin Core,Creator'), array('')); ?>
-                    </div>
-                    <div class="hidden-sm col-md-2">
-                        Subject
-                    </div>
-                    <div class="col-sm-4 col-md-4">
-                        Description
-                    </div>
-                </div>
-            </div>
 			<!-- Image Grid -->
             <?php foreach (loop('items') as $item): ?>
             <div class="item">
