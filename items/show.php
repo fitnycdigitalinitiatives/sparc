62d41c0d018ccf7e916758d69cd3e55ad972c260
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-12-04 15:36:15 -0500

    show

--- items/show.php
+++ items/show.php
@@ -1,51 +1,53 @@
 <?php 
     echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bodyclass' => 'items show'));
 ?>
-
-    <div class="row">
-        <div class="col-sm-12">
-			<!-- Fire OpenSeadragon Separately from other plugins for placement -->
-			<?php echo $this->openseadragon($item->Files); ?>
-        </div>
-	</div>
+	
 	<div class="row">
 		<div class="col-sm-12">
 			<!-- Go to www.addthis.com/dashboard to customize your tools -->
 			<div class="addthis_sharing_toolbox pull-right"></div>
 		</div>
 	</div>
+    <div class="row">
+        <div class="col-sm-12">
+			<!-- Fire OpenSeadragon Separately from other plugins for placement -->
+			<?php echo $this->openseadragon($item->Files); ?>
+        </div>
+	</div>
 	<div class="panel panel-default">
 		<div class="panel-heading">
 			<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
 		</div>
 		<div class="panel-body">
-			<div class="col-sm-6">
-				<?php echo all_element_texts('item'); ?>   
-				<!-- If the item belongs to a collection, the following creates a link to that collection. -->
-				<?php if (metadata('item', 'Collection Name')): ?>
-					<div id="collection" class="element">
-						<h3><?php echo __('Collection'); ?></h3>
-						<div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+			<div class="row">
+				<div class="col-sm-6">
+					<?php echo all_element_texts('item'); ?>   
+					<!-- If the item belongs to a collection, the following creates a link to that collection. -->
+					<?php if (metadata('item', 'Collection Name')): ?>
+						<div id="collection" class="element">
+							<h3><?php echo __('Collection'); ?></h3>
+							<div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+						</div>
+					<?php endif; ?>
+					
+					<!-- The following prints a list of all tags associated with the item -->
+					<?php if (metadata('item', 'has tags')): ?>
+						<div id="item-tags" class="element">
+							<h3><?php echo __('Tags'); ?></h3>
+							<div class="element-text"><?php echo tag_string('item'); ?></div>
+						</div>
+					<?php endif;?>
+					
+					<!-- The following prints a citation for this item. -->
+					<div id="item-citation" class="element">
+						<h3><?php echo __('Citation'); ?></h3>
+						<div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
 					</div>
-				<?php endif; ?>
-				
-				<!-- The following prints a list of all tags associated with the item -->
-				<?php if (metadata('item', 'has tags')): ?>
-					<div id="item-tags" class="element">
-						<h3><?php echo __('Tags'); ?></h3>
-						<div class="element-text"><?php echo tag_string('item'); ?></div>
+					
+					<div id="item-output-formats" class="element">
+						<h3><?php echo __('Output Formats'); ?></h3>
+						<div class="element-text"><?php echo output_format_list(); ?></div>
 					</div>
-				<?php endif;?>
-				
-				<!-- The following prints a citation for this item. -->
-				<div id="item-citation" class="element">
-					<h3><?php echo __('Citation'); ?></h3>
-					<div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
-				</div>
-				
-				<div id="item-output-formats" class="element">
-					<h3><?php echo __('Output Formats'); ?></h3>
-					<div class="element-text"><?php echo output_format_list(); ?></div>
 				</div>
 			</div>
 		</div>
