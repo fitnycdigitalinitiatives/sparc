cf06cc02c15651dbb0b7fa2f2b73cd7435b0609f
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-12-04 16:45:12 -0500

    sidebar

--- items/show.php
+++ items/show.php
@@ -1,57 +1,77 @@
 <?php 
     echo head(array('title' => metadata('item', array('Dublin Core', 'Title')), 'bodyclass' => 'items show'));
 ?>
-	
-	<div class="row">
-		<div class="col-sm-12">
-			<!-- Go to www.addthis.com/dashboard to customize your tools -->
-			<div class="addthis_sharing_toolbox pull-right"></div>
-		</div>
-	</div>
+	<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
     <div class="row">
         <div class="col-sm-12">
 			<!-- Fire OpenSeadragon Separately from other plugins for placement -->
 			<?php echo $this->openseadragon($item->Files); ?>
         </div>
 	</div>
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h1><?php echo metadata('item', array('Dublin Core', 'Title')); ?></h1>
+	<div class="row">
+		<div class="col-sm-12">
+			<!-- Go to www.addthis.com/dashboard to customize your tools -->
+			<div class="addthis_sharing_toolbox pull-right"></div>
 		</div>
-		<div class="panel-body">
-			<div class="row">
-				<div class="col-md-6 col-sm-9">
-					<?php echo all_element_texts('item'); ?>   
-					<!-- If the item belongs to a collection, the following creates a link to that collection. -->
-					<?php if (metadata('item', 'Collection Name')): ?>
-						<div id="collection" class="element">
-							<h3><?php echo __('Collection'); ?></h3>
-							<div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+	</div>
+	<div class="row">
+		<div class="col-md-8">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h2>Metadata</h2>
+				</div>
+				<div class="panel-body">
+					<div class="row">
+						<div class="col-md-6 col-sm-9">
+							<?php echo all_element_texts('item'); ?>   
+							<!-- If the item belongs to a collection, the following creates a link to that collection. -->
+							<?php if (metadata('item', 'Collection Name')): ?>
+								<div id="collection" class="element">
+									<h3><?php echo __('Collection'); ?></h3>
+									<div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
+								</div>
+							<?php endif; ?>
+							
+							<!-- The following prints a list of all tags associated with the item -->
+							<?php if (metadata('item', 'has tags')): ?>
+								<div id="item-tags" class="element">
+									<h3><?php echo __('Tags'); ?></h3>
+									<div class="element-text"><?php echo tag_string('item'); ?></div>
+								</div>
+							<?php endif;?>
+							
+							<!-- The following prints a citation for this item. -->
+							<div id="item-citation" class="element">
+								<h3><?php echo __('Citation'); ?></h3>
+								<div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+							</div>
+							
+							<div id="item-output-formats" class="element">
+								<h3><?php echo __('Output Formats'); ?></h3>
+								<div class="element-text"><?php echo output_format_list(); ?></div>
+							</div>
 						</div>
-					<?php endif; ?>
-					
-					<!-- The following prints a list of all tags associated with the item -->
-					<?php if (metadata('item', 'has tags')): ?>
-						<div id="item-tags" class="element">
-							<h3><?php echo __('Tags'); ?></h3>
-							<div class="element-text"><?php echo tag_string('item'); ?></div>
-						</div>
-					<?php endif;?>
-					
-					<!-- The following prints a citation for this item. -->
-					<div id="item-citation" class="element">
-						<h3><?php echo __('Citation'); ?></h3>
-						<div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
-					</div>
-					
-					<div id="item-output-formats" class="element">
-						<h3><?php echo __('Output Formats'); ?></h3>
-						<div class="element-text"><?php echo output_format_list(); ?></div>
 					</div>
 				</div>
 			</div>
 		</div>
-    </div>
+		<div class="col-md-4">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h2>Related Items</h2>
+				</div>
+				<div class="panel-body">
+					<div class="list-group">
+						<a href="#" class="list-group-item"><div class="media"> <div class="media-left"> <a href="#"> <img class="media-object" src="http://192.168.99.100/files/square_thumbnails/06ef9c0d3c242a4f320bf29dd9a875ff.jpg"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div> </div></a>
+						<a href="#" class="list-group-item"><div class="media"> <div class="media-left"> <a href="#"> <img class="media-object" src="http://192.168.99.100/files/square_thumbnails/06ef9c0d3c242a4f320bf29dd9a875ff.jpg"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div> </div></a>
+						<a href="#" class="list-group-item"><div class="media"> <div class="media-left"> <a href="#"> <img class="media-object" src="http://192.168.99.100/files/square_thumbnails/06ef9c0d3c242a4f320bf29dd9a875ff.jpg"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div> </div></a>
+						<a href="#" class="list-group-item"><div class="media"> <div class="media-left"> <a href="#"> <img class="media-object" src="http://192.168.99.100/files/square_thumbnails/06ef9c0d3c242a4f320bf29dd9a875ff.jpg"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div> </div></a>
+						<a href="#" class="list-group-item"><div class="media"> <div class="media-left"> <a href="#"> <img class="media-object" src="http://192.168.99.100/files/square_thumbnails/06ef9c0d3c242a4f320bf29dd9a875ff.jpg"> </a> </div> <div class="media-body"> <h4 class="media-heading">Media heading</h4> Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. </div> </div></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
     <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
     <ul class="pager">
         <li class="previous"><?php echo link_to_previous_item_show(); ?></li>
