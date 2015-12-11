310edd6615a1cc869876d8fab91b63f11ac138ab
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-12-11 11:54:22 -0500

    exhibit show

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -3,31 +3,36 @@ echo head(array(
     'title' => metadata('exhibit_page', 'title') . ' &middot; ' . metadata('exhibit', 'title'),
     'bodyclass' => 'exhibits show'));
 ?>
-
-<h1><span class="exhibit-page"><?php echo metadata('exhibit_page', 'title'); ?></h1>
-
-<div id="exhibit-blocks">
-<?php exhibit_builder_render_exhibit_page(); ?>
 </div>
+<div class="container-fluid">
+	<h1><?php echo metadata('exhibit_page', 'title'); ?></h1>
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-1" id="exhibit-blocks">
+		<?php exhibit_builder_render_exhibit_page(); ?>
+		</div>
 
-<div id="exhibit-page-navigation">
-    <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
-    <div id="exhibit-nav-prev">
-    <?php echo $prevLink; ?>
-    </div>
-    <?php endif; ?>
-    <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
-    <div id="exhibit-nav-next">
-    <?php echo $nextLink; ?>
-    </div>
-    <?php endif; ?>
-    <div id="exhibit-nav-up">
-    <?php echo exhibit_builder_page_trail(); ?>
-    </div>
-</div>
-
-<nav id="exhibit-pages">
-    <h4><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h4>
-    <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
-</nav>
+		<div class="col-sm-2">
+			<nav id="exhibit-pages">
+				<h4><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h4>
+				<?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
+			</nav>
+		</div>
+	</div>
+	<nav class="text-center">
+		<ul class="pagination">
+			<?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
+			<li id="exhibit-nav-prev">
+			<?php echo $prevLink; ?>
+			</li>
+			<?php endif; ?>
+			<?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
+			<li id="exhibit-nav-next">
+			<?php echo $nextLink; ?>
+			</li>
+			<?php endif; ?>
+			<li class="active" id="exhibit-nav-up">
+			<?php echo exhibit_builder_page_trail(); ?>
+			</li>
+		</ul>
+	</nav>
 <?php echo foot(); ?>
