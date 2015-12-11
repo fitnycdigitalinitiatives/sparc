0b6ea56f3e677205042ab342b0a5d552d1025ca8
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2015-12-11 12:35:55 -0500

    exhibit layout

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -5,34 +5,37 @@ echo head(array(
 ?>
 </div>
 <div class="container-fluid">
-	<h1><?php echo metadata('exhibit_page', 'title'); ?></h1>
 	<div class="row">
-		<div class="col-sm-8 col-sm-offset-1" id="exhibit-blocks">
+		<div class="col-sm-9" id="exhibit-blocks">
 		<?php exhibit_builder_render_exhibit_page(); ?>
 		</div>
 
-		<div class="col-sm-2">
-			<nav id="exhibit-pages">
-				<h4><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h4>
-				<?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
-			</nav>
+		<div class="col-sm-3">
+			<div class="row">
+				<div class="col-sm-9">
+					<nav id="exhibit-pages">
+						<h4><?php echo exhibit_builder_link_to_exhibit($exhibit); ?></h4>
+						<?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
+					</nav>
+				</div>
+			</div>
 		</div>
 	</div>
 	<nav class="text-center">
 		<ul class="pagination">
 			<?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
-			<li id="exhibit-nav-prev">
+			<li>
 			<?php echo $prevLink; ?>
 			</li>
 			<?php endif; ?>
-			<?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
-			<li id="exhibit-nav-next">
-			<?php echo $nextLink; ?>
-			</li>
-			<?php endif; ?>
-			<li class="active" id="exhibit-nav-up">
+			<li class="active">
 			<?php echo exhibit_builder_page_trail(); ?>
 			</li>
+			<?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
+			<li>
+			<?php echo $nextLink; ?>
+			</li>
+			<?php endif; ?>			
 		</ul>
 	</nav>
 <?php echo foot(); ?>
