1d8cf8ce5d0bb5b7e17db415be1c15eb2755a0c0
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-01-07 17:07:34 -0500

    remade homepage/header/footer

--- index.php
+++ index.php
@@ -1,33 +1,17 @@
 <?php echo head(array('bodyid'=>'home')); ?>
 
-<?php echo get_theme_option('Homepage About'); ?>
-<div class="row">
-    <div class="col-sm-4">
-        <?php if (get_theme_option('Display Featured Item') !== '0'): ?>
-            <h2><?php echo __('Featured Item'); ?></h2>
-            <?php echo random_featured_items(1); ?>
-        <?php endif; ?>
-    </div>
-    <div class="col-sm-4">
-        <?php if (get_theme_option('Display Featured Collection') !== '0'): ?>
-            <h2><?php echo __('Featured Collection'); ?></h2>
-            <?php echo random_featured_collection(); ?>
-        <?php endif; ?>
-    </div>
-    <div class="col-sm-4">
-        <?php if ((get_theme_option('Display Featured Exhibit') !== '0') && plugin_is_active('ExhibitBuilder') && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-            <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-        <?php endif; ?>
-    </div>
-</div>    
-<div class="row">
-    <div class="col-sm-12">
-        <h2><?php echo __('Recently Added Items'); ?></h2>
-        <?php echo recent_items(3); ?>
-        <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
-    </div>
-    
-    <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
+<div class="jumbotron">
+	<h1>SPARC Digital</h1>
+	<h2>The digital collections of the FIT Library's Special Collections and College Archives</h2>
+	<!-- Search -->
+	<form role="search" action="/search">
+		<div class="input-group input-group-lg">
+			<input type="text" name="query" id="query" value="" class="form-control" placeholder="Search the collection">        
+			<span class="input-group-btn">
+				<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
+			</span>
+		</div>
+	</form>
 </div>
 
 <?php echo foot(); ?>
