23dab4ed1d36340532f2616e51632446135f971e
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-01-13 17:22:35 -0500

    fixed pagination

--- common/pagination_control.php
+++ common/pagination_control.php
@@ -1,40 +1,53 @@
-<?php if ($this->pageCount > 1): ?>
+<?php
+if ($this->pageCount > 1):
+    $getParams = $_GET;
+?>
 <nav class="text-center">
     <ul class="pagination">
-        <?php if ($this->first != $this->current): ?>
+        <?php if (($this->first != $this->current) && !(in_array($this->first, $this->pagesInRange))): ?>
         <!-- First page link --> 
         <li class="pagination_first">
-        <a href="<?php echo html_escape($this->url(array('page' => $this->first), null, $_GET)); ?>"><?php echo __('First'); ?></a>
+		<?php $getParams['page'] = $first; ?>
+        <a href="<?php echo html_escape($this->url(array(), null, $getParams)); ?>"><?php echo __('First'); ?></a>
         </li>
         <?php endif; ?>
         
         <?php if (isset($this->previous)): ?>
         <!-- Previous page link --> 
         <li class="pagination_previous">
-        <a href="<?php echo html_escape($this->url(array('page' => $this->previous), null, $_GET)); ?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
+		<?php $getParams['page'] = $previous; ?>
+        <a href="<?php echo html_escape($this->url(array(), null, $getParams)); ?>" aria-label="Previous"><span aria-hidden="true">&laquo;</span></a>
         </li>
         <?php endif; ?>
         
         <!-- Numbered page links -->
         <?php foreach ($this->pagesInRange as $page): ?> 
         <?php if ($page != $this->current): ?>
-        <li class="pagination_range"><a href="<?php echo html_escape($this->url(array('page' => $page), null, $_GET)); ?>"><?php echo $page; ?></a></li>
+        <li class="pagination_range">
+		<?php $getParams['page'] = $page; ?>
+		<a href="<?php echo html_escape($this->url(array(), null, $getParams)); ?>"><?php echo $page; ?></a>
+		</li>
         <?php else: ?>
-        <li class="active"><a href="<?php echo html_escape($this->url(array('page' => $page), null, $_GET)); ?>"><?php echo $page; ?><span class="sr-only">(current)</span></a></li>
+        <li class="active">
+		<?php $getParams['page'] = $page; ?>
+		<a href="<?php echo html_escape($this->url(array(), null, $getParams)); ?>"><?php echo $page; ?><span class="sr-only">(current)</span></a>
+		</li>
         <?php endif; ?>
         <?php endforeach; ?>
         
         <?php if (isset($this->next)): ?> 
         <!-- Next page link -->
         <li class="pagination_next">
-        <a href="<?php echo html_escape($this->url(array('page' => $this->next), null, $_GET)); ?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
+		<?php $getParams['page'] = $next; ?>
+        <a href="<?php echo html_escape($this->url(array(), null, $getParams)); ?>" aria-label="Next"><span aria-hidden="true">&raquo;</span></a>
         </li>
         <?php endif; ?>
         
-        <?php if ($this->last != $this->current): ?>
+        <?php if (($this->last != $this->current) && !(in_array($this->last, $this->pagesInRange))): ?>
         <!-- Last page link --> 
         <li class="pagination_last">
-        <a href="<?php echo html_escape($this->url(array('page' => $this->last), null, $_GET)); ?>"><?php echo __('Last'); ?></a>
+		<?php $getParams['page'] = $last; ?>
+        <a href="<?php echo html_escape($this->url(array(), null, $getParams)); ?>"><?php echo __('Last'); ?></a>
         </li>
         <?php endif; ?>
     </ul>
