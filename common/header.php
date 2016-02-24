8055cd1aa9578ea38bbdf6b1b52226f1c83b1d59
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-02-24 11:48:36 -0500

    homepage

--- common/header.php
+++ common/header.php
@@ -57,7 +57,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="/omeka">
+				<a class="navbar-brand" href="/sparc">
 					<?php echo '<img src="' . img('fit_sparc.png') . '" alt="Brand">'; ?>
 				</a>
 			</div>
