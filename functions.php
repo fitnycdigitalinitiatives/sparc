e89ef66bec4d9481602a7109a2432061586f9991
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-07-20 15:28:59 -0400

    testing

--- functions.php
+++ functions.php
@@ -210,7 +210,6 @@ function palette($current_item)
 	if (metadata('item', array('Item Type Metadata', 'Color Data'))) {
 		$palette = metadata($current_item, array('Item Type Metadata', 'Color Data'));
 		$html = '<ul class="list-inline">';
-		return json_decode(html_entity_decode($palette), true);
 		foreach (json_decode(html_entity_decode($palette), true) as $section) {
 			$color = $section["color"];
 			$closest = $section['closest'];
