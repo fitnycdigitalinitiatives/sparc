3311f3cc5a45e7b46122eccc84733ef750c47afc
Joseph Anderson <joseph_anderson@fitnyc.edu>
2016-09-23 15:40:01 -0400

    fixed collections

--- collections/browse.php
+++ collections/browse.php
@@ -31,20 +31,19 @@
 	<div class="row" id="grid">
 		<?php foreach (loop('collections') as $collection): ?>
 		<div class="col-md-4 col-sm-6 item-thumb">
-			<?php if ($collectionImage = record_image('collection', 'square_thumbnail', array('class' => 'img-responsive'))): ?>
-			<?php echo link_to_items_browse($collectionImage . '<div class="caption"><h5>' . metadata('collection', array('Dublin Core', 'Title')) . '</h5></div>', array('collection' => metadata($collection, 'id')), array('class' => 'thumbnail')); ?>
-			<?php endif; ?>
+      <?php $item = get_records('Item', array('collection' => metadata($collection, 'id')), 1); ?>
+      <?php echo link_to_items_browse(mdid_thumbnail_tag($item, 'img-responsive') . '<div class="caption"><h5>' . metadata($collection, array('Dublin Core', 'Title')) . '</h5></div>', array('collection' => metadata($collection, 'id')), array('class' => 'thumbnail')); ?>
 			<?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
 		</div>
 		<?php endforeach; ?>
 	</div>
 	<?php echo pagination_links(); ?>
-	
+
 	<?php else : ?>
 	<p><?php echo 'No collections added, yet.'; ?></p>
 	<?php endif; ?>
 </div>
-     
+
 
 <?php fire_plugin_hook('public_collections_browse', array('collections'=> $collections, 'view' => $this)); ?>
 <?php echo foot(); ?>
