2d546823fedd7326b2f7be4978086744b8733d95
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-12-13 11:00:55 -0500

    navbar default open

--- exhibit-builder/exhibits/exhibit_footer.php
+++ exhibit-builder/exhibits/exhibit_footer.php
@@ -4,7 +4,7 @@
       <nav class="navbar navbar-inverse navbar-static-bottom">
         <div class="container">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sub-navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sub-navbar" aria-expanded="true" aria-controls="navbar">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
@@ -12,7 +12,7 @@
             </button>
             <?php echo exhibit_builder_link_to_exhibit($exhibit, null, array('class' => 'navbar-brand')); ?>
           </div>
-          <div id="sub-navbar" class="navbar-collapse collapse">
+          <div id="sub-navbar" class="navbar-collapse collapse in">
             <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
           </div><!--/.nav-collapse -->
         </div>
