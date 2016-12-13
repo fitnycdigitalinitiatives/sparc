6d49d84ca7750bc7c88fcf0fbc58679900c7e4f1
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2016-12-13 10:46:30 -0500

    exhibits

--- exhibit-builder/exhibits/exhibit_footer.php
+++ exhibit-builder/exhibits/exhibit_footer.php
@@ -4,7 +4,7 @@
       <nav class="navbar navbar-inverse navbar-static-bottom">
         <div class="container">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#sub-navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sub-navbar" aria-expanded="false" aria-controls="navbar">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
