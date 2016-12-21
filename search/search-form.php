79027e9732a46e3fc12e016e16e88b34c935fb42
Joseph Anderson <joseph_anderson@e-e413-m01.dt.fitsuny.edu>
2016-12-21 17:30:38 -0500

    Revert "testing"
    This reverts commit cde8ab04287cacf9c26ceed6129d8ce8fb818cc3.

--- search/search-form.php
+++ search/search-form.php
@@ -1,6 +1,5 @@
 <?php echo $this->form('search-form', $options['form_attributes']); ?>
     <div class="input-group">
-      <?php echo $filters['query']; ?>
         <?php echo $this->formText('query', $filters['query'], array('class'=>'form-control', 'placeholder'=>'Search the collection')); ?>
         <span class="input-group-btn">
 			<button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
