5af00b4885975cbddc4e4d470363c7f65ba1c503
Joseph Anderson <joseph_anderson@e-e413-m01.dt.fitsuny.edu>
2016-12-21 17:47:53 -0500

    search button

--- search/search-form.php
+++ search/search-form.php
@@ -2,7 +2,7 @@
     <div class="input-group">
         <?php echo $this->formText('query', $filters['query'], array('class'=>'form-control', 'placeholder'=>'Search the collection')); ?>
         <span class="input-group-btn">
-			<button class="btn btn-default" type="button" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
+			<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
 		</span>
     </div>
 </form>
