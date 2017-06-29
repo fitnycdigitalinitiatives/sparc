ff8cf24f639d3548dd562625899885d642dd804e
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-06-29 14:01:05 -0400

    background image

--- exhibit-builder/exhibits/summary.php
+++ exhibit-builder/exhibits/summary.php
@@ -1,7 +1,7 @@
 <?php echo head(array('title' => metadata('exhibit', 'title'), 'bodyclass'=>'exhibits summary')); ?>
 
-  <div class="row">
-		<div class="col-sm-7">
+  <div class="row summary-text">
+		<div class="col-sm-12">
 			<h1><?php echo metadata('exhibit', 'title'); ?></h1>
 			<?php if ($exhibitDescription = metadata('exhibit', 'description', array('no_escape' => true))): ?>
 				<div class="exhibit-description text-justify">
@@ -17,16 +17,14 @@
 			<?php endif; ?>
 			<?php $firstPage = $exhibit->getFirstTopPage();; ?>
 		</div>
-		<div class="col-sm-5 exhibition-thumb">
-			<?php if ($item = get_exhibit_item ($exhibit)): ?>
-				<?php echo exhibit_builder_link_to_exhibit($exhibit, mdid_thumbnail_tag($item, 'img-responsive'), array('class' => 'thumbnail'), $firstPage); ?>
-			<?php endif; ?>
-		</div>
 	</div>
   <div class="row summary-nav">
 		<div class="col-xs-12">
       <?php echo exhibit_builder_link_to_exhibit($exhibit, '<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round next', 'role' => 'button'), $firstPage); ?>
     </div>
   </div>
+  <?php $bg_image = metadata('exhibit', 'slug') + '_exhibition.jpg'; ?>
+  <div id="exhibition_background" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("<?php echo img($bg_image); ?>");"></div>
+
 
 <?php echo common('exhibit_footer', array('exhibit' => $exhibit, 'exhibit_page' => null), 'exhibit-builder/exhibits'); ?>
