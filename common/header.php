0cb60e66537ffdbb82cf0a06c2e0efd87a56f30c
JOSEPH ANDERSON <JOSEPH_ANDERSON@fitsuny.edu>
2017-06-29 10:48:31 -0400

    test for addthis script

--- common/header.php
+++ common/header.php
@@ -37,7 +37,10 @@
         queue_js_file('globals');
         echo head_js();
     ?>
-	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5661dc9df1aebb59" async="async"></script>
+  <?php if (@$bodyclass == 'items show'):  ?>
+    <!-- Go to www.addthis.com/dashboard to customize your tools -->
+    <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-55c8b7772f2446eb"></script>
+  <?php endif; ?>
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
