d7e2efa37ae0c5d6f39d52dfb6b0f4ef000badba
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-06-29 14:12:49 -0400

    testing

--- exhibit-builder/exhibits/summary.php
+++ exhibit-builder/exhibits/summary.php
@@ -27,8 +27,9 @@
   <?php
     $slug = $exhibit->slug;
     $bg_image = $slug + '_exhibition.jpg';
+    echo $bg_image;
   ?>
-  <div id="exhibition_background" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("<?php echo img($bg_image); ?>");"></div>
+  <div id="exhibition_background" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url("");"></div>
 
 
 <?php echo common('exhibit_footer', array('exhibit' => $exhibit, 'exhibit_page' => null), 'exhibit-builder/exhibits'); ?>
