75845c44803bbe57de7aea5fb96bdef251b6d122
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-05 17:22:12 -0400

    nav trails

--- exhibit-builder/exhibits/exhibit_footer.php
+++ exhibit-builder/exhibits/exhibit_footer.php
@@ -15,6 +15,36 @@
           <div id="sub-navbar" class="navbar-collapse collapse in .visible-lg-block .visible-xs-block">
             <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
           </div><!--/.nav-collapse -->
+          <!-- Just Prev, next, current pages -->
+          <div id="sub-navbar" class="navbar-collapse collapse in .hidden-lg-block .hidden-xs-block">
+            <?php if (!$exhibit_page): ?>
+              <?php $firstPage = $exhibit->getFirstTopPage(); ?>
+              <li role="presentation">
+                <?php echo exhibit_builder_link_to_exhibit($exhibit, metadata($firstPage, 'title'), array(), $firstPage); ?>
+              </li>
+              <?php $nextLink = exhibit_builder_link_to_next_page(); ?>
+              <li role="presentation">
+                <?php echo @$prevLink; ?>
+              </li>
+              <p class="navbar-text"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></p>
+            <?php else: ?>
+              <?php if ($prevLink = exhibit_builder_link_to_previous_page()): ?>
+                <p class="navbar-text"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></p>
+                <li role="presentation">
+                  <?php echo $prevLink; ?>
+                </li>
+              <?php endif; ?>
+              <li role="presentation" class="active">
+                <?php echo exhibit_builder_link_to_exhibit($exhibit, metadata($exhibit_page, 'title'), array(), $exhibit_page); ?>
+              </li>
+              <?php if ($nextLink = exhibit_builder_link_to_next_page()): ?>
+                <li role="presentation">
+                  <?php echo $nextLink; ?>
+                </li>
+                <p class="navbar-text"><span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span></p>
+              <?php endif; ?>
+            <?php endif; ?>
+          </div><!--/.nav-collapse -->
         </div>
       </nav>
       <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
