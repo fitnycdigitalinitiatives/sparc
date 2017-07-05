0b6c23969a1631600ce968c76b3da0ee76374d4a
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-05 12:49:17 -0400

    revert subnavbar

--- exhibit-builder/exhibits/exhibit_footer.php
+++ exhibit-builder/exhibits/exhibit_footer.php
@@ -3,10 +3,6 @@
     <footer class="footer" role="contentinfo">
       <nav class="navbar navbar-inverse navbar-static-bottom">
         <div class="container">
-          <!--Reverse Menu for bottom of screen -->
-          <div id="sub-navbar" class="navbar-collapse collapse">
-            <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
-          </div><!--/.nav-collapse -->
           <div class="navbar-header">
             <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#sub-navbar" aria-expanded="true" aria-controls="navbar">
               <span class="sr-only">Toggle navigation</span>
@@ -16,6 +12,9 @@
             </button>
             <?php echo exhibit_builder_link_to_exhibit($exhibit, null, array('class' => 'navbar-brand')); ?>
           </div>
+          <div id="sub-navbar" class="navbar-collapse collapse in">
+            <?php echo exhibit_builder_page_tree($exhibit, $exhibit_page); ?>
+          </div><!--/.nav-collapse -->
         </div>
       </nav>
       <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
