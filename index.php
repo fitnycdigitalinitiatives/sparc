324cef53c1f0ebfdf6f712cda09a441d91d4c547
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-07 11:07:57 -0400

    homepage fixes

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	<!-- Get filter for plugin use -->
 	<?php $url = apply_filters('search_form_default_action', url('search')); ?>
 	<form id="search-form" name="search-form" role="search" action="<?php echo $url; ?>" method="get">
-		<div class="input-group input-group-lg">
+		<div class="input-group input-group-md">
 			<input type="text" name="query" id="query" value="" class="form-control" placeholder="Search the collection">
 			<span class="input-group-btn">
 				<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
