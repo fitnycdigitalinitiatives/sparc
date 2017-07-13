8a2f1fd9b373ef10d23868f3b348b00cbbf6d8aa
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-13 16:52:54 -0400

    about page

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -10,13 +10,25 @@ echo head(array(
     'bodyid' => metadata('simple_pages_page', 'slug')
 ));
 ?>
-<div class="row">
+<div class="row" id="primary">
   <div class="col-sm-8">
-    <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
+    <h1 class="page-header"><?php echo metadata('simple_pages_page', 'title'); ?></h1>
     <?php
     $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
     echo $this->shortcodes($text);
     ?>
+  </div>
+  <div class="col-sm-3 col-sm-offset-1">
+    <div class="panel panel-default">
+      <!-- Default panel contents -->
+      <div class="panel-heading">Links</div>
+      <!-- List group -->
+      <ul class="list-group">
+        <a href="https://www.fitnyc.edu/library/sparc/index.php" class="list-group-item">SPARC Homepage</a>
+        <a href="http://blog.fitnyc.edu/materialmode/" class="list-group-item">Material Mode: SPARC's Blog</a>
+        <a href="https://atom-sparc.fitnyc.edu/" class="list-group-item">SPARC Connect</a>
+      </ul>
+    </div>
   </div>
 </div>
 
