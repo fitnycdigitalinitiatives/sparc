04b7d437b9395bdde270285f1eec72737f617029
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-19 15:25:37 -0400

    lists

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -4,9 +4,13 @@
       <?php if (($setName == "Dublin Core") and (($elementName == "Subject") or ($elementName == "Creator") or ($elementName == "Is Part Of") or ($elementName == "Contributor") or ($elementName == "Source") or ($elementName == "Format") or ($elementName == "Medium") or ($elementName == "Spatial Coverage") or ($elementName == "Temporal Coverage"))): ?>
         <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
             <dt><?php echo html_escape(__($elementName)); ?></dt>
-            <?php foreach ($elementInfo['texts'] as $text): ?>
-                <dd class="element-text"><?php echo tag_search ($text); ?></dd>
-            <?php endforeach; ?>
+            <dd class="element-text">
+              <ul class="list-unstyled">
+                <?php foreach ($elementInfo['texts'] as $text): ?>
+                    <li><?php echo tag_search ($text); ?></li>
+                <?php endforeach; ?>
+              </ul>
+            </dd>
         </div><!-- end element -->
       <?php else: ?>
         <div id="<?php echo text_to_id(html_escape("$setName $elementName")); ?>" class="element">
