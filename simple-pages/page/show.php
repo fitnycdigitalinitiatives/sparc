c4ed7b5edb3e526948c5182f03d35eb7756b920d
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-07-25 16:59:07 -0400

    removed row

--- simple-pages/page/show.php
+++ simple-pages/page/show.php
@@ -10,7 +10,7 @@ echo head(array(
     'bodyid' => metadata('simple_pages_page', 'slug')
 ));
 ?>
-<div class="row" id="primary">
+<div id="primary">
   <h1><?php echo metadata('simple_pages_page', 'title'); ?></h1>
   <?php
   $text = metadata('simple_pages_page', 'text', array('no_escape' => true));
