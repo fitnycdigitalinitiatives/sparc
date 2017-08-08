5a85029a8ea852968077351d573eedeb49b12aab
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-08-08 15:59:24 -0400

    fixing exhibit builder bg

--- exhibit-builder/exhibits/summary.php
+++ exhibit-builder/exhibits/summary.php
@@ -27,9 +27,10 @@
   <?php
     $slug = $exhibit->slug;
     $bg_image = $slug . '_exhibition.jpg';
-    $bg_image_url = img($bg_image);
+    if ($bg_image_url = img($bg_image)):
   ?>
-  <div id="exhibition_background" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo $bg_image_url; ?>');"></div>
+    <div id="exhibition_background" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('<?php echo $bg_image_url; ?>');"></div>
+  <?php endif; ?>
 
 
 <?php echo common('exhibit_footer', array('exhibit' => $exhibit, 'exhibit_page' => null), 'exhibit-builder/exhibits'); ?>
