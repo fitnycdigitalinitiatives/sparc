6344919bb11cda54b263d166260be7a16563b9ec
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-08-25 13:48:20 -0400

    error page

--- error/404.php
+++ error/404.php
@@ -1,7 +1,12 @@
 <?php
 $pageTitle = __('404 Page Not Found');
-echo head(array('title'=>$pageTitle));
+echo head(array('title'=>$pageTitle,'bodyclass' => 'error'));
 ?>
-<h1><?php echo $pageTitle; ?></h1>
-<p><?php echo __('%s is not a valid URL.', html_escape($badUri)); ?></p>
+<div class="row">
+  <div class="col-sm-4">
+    <h1>Oh Dear...</h1>
+    <h2>We're not quite sure how to break this to you, but it seems the page you are looking for doesn't actually exist.</h2>
+    <p class="lead">If you continue to run into this sort of problem, please <a href="mailto:fitlibsparc@fitnyc.edu">contact us</a> immediately and we'll see what we can to do to sort it all out.</p>
+  </div>
+</div>
 <?php echo foot(); ?>
