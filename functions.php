64fa866ae72f3b908bfbb690386ab439e7fe4a77
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-09-06 11:08:41 -0400

    better related items algorithm

--- functions.php
+++ functions.php
@@ -197,19 +197,33 @@ function item_search_filters_bootstrap(array $params = null)
 function related_items($current_item)
 {
   if (($collection = get_collection_for_item($current_item)) && ($subject_1 = metadata($current_item, array('Dublin Core', 'Subject'), array('index' => 0, 'no_escape' => true)))) {
-		$related_items_1 = get_records('Item', array('collection' => metadata($collection, 'id'), 'tags' => $subject_1, 'sort_field' => 'random'), 7);
+		$related_items_1 = get_records('Item', array('collection' => metadata($collection, 'id'), 'tags' => $subject_1, 'sort_field' => 'random'), 5);
 	}
   else {
     $related_items_1 = array();
   }
   if (($collection = get_collection_for_item($current_item)) && ($subject_2 = metadata($current_item, array('Dublin Core', 'Subject'), array('index' => 1, 'no_escape' => true)))) {
-		$related_items_2 = get_records('Item', array('collection' => metadata($collection, 'id'), 'tags' => $subject_2, 'sort_field' => 'random'), 7);
+		$related_items_2 = get_records('Item', array('collection' => metadata($collection, 'id'), 'tags' => $subject_2, 'sort_field' => 'random'), 5);
 	}
   else {
     $related_items_2 = array();
   }
-  if (($related_items_1) || ($related_items_2)) {
-    $related_items = array_merge(@$related_items_1, @$related_items_2);
+  if ($subject_1 = metadata($current_item, array('Dublin Core', 'Subject'), array('index' => 0, 'no_escape' => true))) {
+		$related_items_3 = get_records('Item', array('tags' => $subject_1, 'sort_field' => 'random'), 5);
+	}
+  else {
+    $related_items_3 = array();
+  }
+  if ($medium = metadata($current_item, array('Dublin Core', 'Medium'), array('index' => 0, 'no_escape' => true))) {
+		$related_items_4 = get_records('Item', array('tags' => $medium, 'sort_field' => 'random'), 5);
+	}
+  else {
+    $related_items_4 = array();
+  }
+  if (($related_items_1) || ($related_items_2) || ($related_items_3) || ($related_items_4)) {
+    $related_items = array_merge(@$related_items_1, @$related_items_2, @$related_items_3, @$related_items_4);
+    $related_items = array_unique($related_items);
+    $related_items = array_slice($related_items, 0, 7);
     if ($related_items) {
       $html = '<div class="col-md-4 related-items"><div class="panel panel-default"><div class="panel-heading"><h4>Related Items</h4></div><div class="list-group">';
       foreach ($related_items as $related_item) {
