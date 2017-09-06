01a6e481ec6792121a3ae1996b67fc9cad79f2a4
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-09-06 15:29:24 -0400

    ada stuff

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 	<?php $url = apply_filters('search_form_default_action', url('search')); ?>
 	<form id="search-form" name="search-form" role="search" action="<?php echo $url; ?>" method="get">
 		<div class="input-group input-group-md">
-			<input type="text" name="query" id="query" value="" class="form-control" placeholder="Search the collection">
+			<input type="text" name="query" id="query" value="" class="form-control" placeholder="Search the collection" aria-label="Search">
 			<span class="input-group-btn">
 				<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
 			</span>
--- search/search-form.php
+++ search/search-form.php
@@ -1,6 +1,6 @@
 <?php echo $this->form('search-form', $options['form_attributes']); ?>
     <div class="input-group">
-        <?php echo $this->formText('query', $filters['query'], array('class'=>'form-control', 'placeholder'=>'Search the collection')); ?>
+        <?php echo $this->formText('query', $filters['query'], array('class'=>'form-control', 'placeholder'=>'Search the collection', 'aria-label' => 'Search')); ?>
         <span class="input-group-btn">
 			<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span></button>
 		</span>
