5b1ed0242fd03372209bedfd93b6fa1e636c73b5
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-09-07 15:21:09 -0400

    no search results

--- items/browse.php
+++ items/browse.php
@@ -31,8 +31,38 @@
           <?php echo browse_sort_links_bootstrap($sortLinks); ?>
         </div>
       </div>
-    <?php else: ?>
+    <?php elseif ($total_results == 0): ?>
       <!-- No Results -->
+      <div class="col-md-3 col-sm-4 col-xs-8">
+        <h4>Showing <?php echo $total_results; ?> results for <em><?php echo $isfb; ?></em></h4>
+      </div>
+      <div class="col-md-6 col-sm-4 hidden-xs" id="color-bar">
+        <?php echo basic_color_board(); ?>
+      </div>
+      <div class="col-md-3 col-xs-4">
+        <div id="sort_button" class="dropdown pull-right">
+          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Sort by:
+            <span class="caret"></span>
+          </button>
+          <?php
+            $sortLinks[__('Title')] = 'Dublin Core,Title';
+            $sortLinks[__('Creator')] = 'Dublin Core,Creator';
+            $sortLinks[__('Date')] = 'Dublin Core,Date';
+            $sortLinks[__('Date Added')] = 'added';
+            ?>
+          <?php echo browse_sort_links_bootstrap($sortLinks); ?>
+        </div>
+        <div id="color_button" class="dropdown pull-right visible-xs-block">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Color Family <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" id="color_board">
+            <li><?php echo basic_color_board(); ?></li>
+          </ul>
+        </div>
+      </div>
+    <?php else: ?>
+      <!-- General Browse -->
       <div class="col-md-3 col-sm-4 col-xs-8">
         <h4>Showing <?php echo $total_results; ?> items total</h4>
       </div>
@@ -78,8 +108,12 @@
 		</div>
 		<?php echo pagination_links(); ?>
 	<?php else : ?>
-    <h1>Oh Dear...</h1>
-    <p class="lead">We regret to inform you that there are no items at this time that match your search. Please try browsing by another color or searching by a term.</p>
+    <div class="row">
+      <div class="col-sm-4 col-xs-6">
+        <h1>Oh Dear...</h1>
+        <p class="lead">We regret to inform you that there are no items at this time that match your search. Please try browsing by another color or searching by a term.</p>
+      </div>
+    </div>
 	<?php endif; ?>
 	</div>
 
