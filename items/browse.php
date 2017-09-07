7969e930d9690a19dadce1ca905b5fbcd73e270b
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-09-07 10:51:16 -0400

    working on color grid

--- items/browse.php
+++ items/browse.php
@@ -37,6 +37,57 @@
     </div>
   </div>
 
+  <div class="row results">
+    <?php if ($isfb = item_search_filters_bootstrap()): ?>
+      <div class="col-xs-8">
+        <h4>Showing <?php echo $total_results; ?> results for <em><?php echo $isfb; ?></em></h4>
+      </div>
+      <div class="col-xs-4">
+        <div id="sort_button" class="dropdown pull-right">
+          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Sort by:
+            <span class="caret"></span>
+          </button>
+          <?php
+            $sortLinks[__('Title')] = 'Dublin Core,Title';
+            $sortLinks[__('Creator')] = 'Dublin Core,Creator';
+            $sortLinks[__('Date')] = 'Dublin Core,Date';
+            $sortLinks[__('Date Added')] = 'added';
+            ?>
+          <?php echo browse_sort_links_bootstrap($sortLinks); ?>
+        </div>
+      </div>
+    <?php else: ?>
+      <div class="col-sm-4 col-xs-8">
+        <h4>Showing <?php echo $total_results; ?> items total</h4>
+      </div>
+      <div class="col-sm-4 hidden-xs">
+      </div>
+      <div class="col-xs-4">
+        <div id="sort_button" class="dropdown pull-right">
+          <button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+            Sort by:
+            <span class="caret"></span>
+          </button>
+          <?php
+            $sortLinks[__('Title')] = 'Dublin Core,Title';
+            $sortLinks[__('Creator')] = 'Dublin Core,Creator';
+            $sortLinks[__('Date')] = 'Dublin Core,Date';
+            $sortLinks[__('Date Added')] = 'added';
+            ?>
+          <?php echo browse_sort_links_bootstrap($sortLinks); ?>
+        </div>
+        <div id="color_button" class="dropdown pull-right visible-xs-block">
+          <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Color Family <span class="caret"></span>
+          </button>
+          <ul class="dropdown-menu" id="color_board">
+            <li><?php echo basic_color_board(); ?></li>
+          </ul>
+        </div>
+      </div>
+    <?php endif; ?>
+  </div>
+
 
 	<div class="browse-items">
 	<?php if ($total_results > 0): ?>
