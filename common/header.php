fdfda71461273951f618b5725bdb86e9a9bf118d
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-09-26 15:40:22 -0400

    analytics

--- common/header.php
+++ common/header.php
@@ -36,6 +36,15 @@
         queue_js_file('globals');
         echo head_js();
     ?>
+    <!-- Global Site Tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107096384-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments)};
+      gtag('js', new Date());
+
+      gtag('config', 'UA-107096384-1');
+    </script>
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
