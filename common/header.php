97f49b71ca8a81cee0f16efc321adc812554fec9
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-10-04 17:56:10 -0400

    testing search

--- common/header.php
+++ common/header.php
@@ -81,7 +81,7 @@
     source: tags,
     templates: {
       suggestion: function(data){
-            return '<div>' + data + ' is eating you</div>';
+            return '<div><a href="/solr-search?q=%28' + data + '%29">' + data + '</a></div>';
       }
     }
     });
