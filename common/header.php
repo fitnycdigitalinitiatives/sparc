0db66e85ea3a16d886886e925a76ad6d1a111b03
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-10-11 15:35:34 -0400

    testing

--- common/header.php
+++ common/header.php
@@ -83,7 +83,7 @@
     limit: 7,
     templates: {
       suggestion: function(data){
-            return '<a href="/solr-search?q=%28' + encodeURIComponent(data.tag) + '%29"><div>' + data.tag + '</div></a>';
+            return '<a href="/solr-search?q=%28' + encodeURIComponent(data) + '%29"><div>' + data + '</div></a>';
       }
     }
     });
