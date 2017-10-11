3b37dfb54f115f8f0b097927db5474819584a6e6
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-10-11 17:29:38 -0400

    cleaning up search form

--- search/search-form.php
+++ search/search-form.php
@@ -1,8 +1,6 @@
 <?php echo $this->form('search-form', $options['form_attributes']); ?>
-    <div class="input-group">
+    <div class="form-group has-feedback">
         <?php echo $this->formText('query', $filters['query'], array('class'=>'form-control', 'placeholder'=>'Search the collection', 'aria-label' => 'Search')); ?>
-        <span class="input-group-btn">
-			<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span><span class="sr-only">Search</span></button>
-		</span>
+        <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
     </div>
 </form>
