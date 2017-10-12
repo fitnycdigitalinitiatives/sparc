96ceaa70a0769e0d1e909b6ed1d9031b9c72b4b1
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-10-12 15:48:32 -0400

    updating style name

--- common/header.php
+++ common/header.php
@@ -24,7 +24,7 @@
     <?php
         queue_css_url('//fonts.googleapis.com/css?family=Archivo+Narrow:400,400italic,700,700italic');
         queue_css_file('lib/bootstrap.min');
-        queue_css_file('style-1');
+        queue_css_file('style-2');
         queue_css_file('fonts/font-awesome/css/font-awesome.min');
         echo head_css();
     ?>
