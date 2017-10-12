923e15e7f3c7c5e949d538a31a6065f6070f4103
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-10-12 13:12:09 -0400

    Revert "Merge autocomplete"
    This reverts commit a5f53b96faaedd1cd4136e570bd162c00bcc0ad2.

--- index.php
+++ index.php
@@ -7,9 +7,11 @@
 	<!-- Get filter for plugin use -->
 	<?php $url = apply_filters('search_form_default_action', url('search')); ?>
 	<form id="search-form" name="search-form" role="search" action="<?php echo $url; ?>" method="get">
-		<div class="form-group has-feedback">
+		<div class="input-group input-group-md">
 			<input type="text" name="query" id="query" value="" class="form-control" placeholder="Search the collection" aria-label="Search">
-			<span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+			<span class="input-group-btn">
+				<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span><span class="sr-only">Search</span></button>
+			</span>
 		</div>
 	</form>
 </div>
--- search/search-form.php
+++ search/search-form.php
@@ -1,6 +1,8 @@
 <?php echo $this->form('search-form', $options['form_attributes']); ?>
-    <div class="form-group has-feedback">
+    <div class="input-group">
         <?php echo $this->formText('query', $filters['query'], array('class'=>'form-control', 'placeholder'=>'Search the collection', 'aria-label' => 'Search')); ?>
-        <span class="glyphicon glyphicon-search form-control-feedback" aria-hidden="true"></span>
+        <span class="input-group-btn">
+			<button class="btn btn-default" name="submit_search" id="submit_search" type="submit" value="Search"><span class="glyphicon glyphicon-search" aria-label="search"></span><span class="sr-only">Search</span></button>
+		</span>
     </div>
 </form>
