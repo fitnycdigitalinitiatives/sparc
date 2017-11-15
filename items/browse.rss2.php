84961b4769afa26090e8aa35ee5c308e66f26f94
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-11-15 11:52:59 -0500

    testing

--- items/browse.rss2.php
+++ items/browse.rss2.php
@@ -6,13 +6,4 @@ set_current_record('item', $item, true);
 $entry['title'] = metadata($item, 'display_title', array('no_escape' => true));
 $entry['link'] = xml_escape(record_url($item, null, true));
 $entry['lastUpdate'] = strtotime($item->added);
-//List the first file as an enclosure (only one per RSS feed)
-if (($files = $item->Files) && ($file = current($files))) {
-    $entry['enclosure'] = array();
-    $fileDownloadUrl = file_display_url($file);
-    $enc['url'] = $fileDownloadUrl;
-    $enc['type'] = $file->mime_type;
-    $enc['length'] = (int) $file->size;
-    $entry['enclosure'][] = $enc;
-}
-return $entry;
+echo $entry;
