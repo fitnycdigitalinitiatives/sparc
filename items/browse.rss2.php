b4504dfbd44d7cb6addc545e7d7494e4a0d1a1f9
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-11-15 11:58:30 -0500

    testing

--- items/browse.rss2.php
+++ items/browse.rss2.php
@@ -2,5 +2,5 @@
 $item = $items[0];
 $entry = array();
 set_current_record('item', $item, true);
-$entry['title'] = metadata($item, 'display_title', array('no_escape' => true));
+$entry['title'] = metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true));
 echo all_element_texts($item);
