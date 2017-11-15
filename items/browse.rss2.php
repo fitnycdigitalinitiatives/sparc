b17bf637a64466a82e1a5f59bebf9991615b1efd
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-11-15 11:54:45 -0500

    testing

--- items/browse.rss2.php
+++ items/browse.rss2.php
@@ -1,9 +1,3 @@
 <?php
 $item = $items[0];
 echo metadata($item, 'display_title', array('no_escape' => true));
-$entry = array();
-// Title is a CDATA section, so no need for extra escaping.
-$entry['title'] = metadata($item, 'display_title', array('no_escape' => true));
-$entry['link'] = xml_escape(record_url($item, null, true));
-$entry['lastUpdate'] = strtotime($item->added);
-echo $entry;
