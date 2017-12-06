4c8bb8c17443c734a8813e9c33c0b06e550d6bae
fitnycdigitalinitiatives <joseph_anderson@fitnyc.edu>
2017-12-06 15:36:25 -0500

    adding creator

--- functions.php
+++ functions.php
@@ -501,6 +501,7 @@ class Output_ItemRss2_Custom
         // Title is a CDATA section, so no need for extra escaping.
         $entry['title'] = metadata($item, array('Dublin Core', 'Title'), array('no_escape' => true));
         $entry['description'] = $this->buildDescription_custom($item);
+        $entry['author'] = metadata($item, array('Dublin Core', 'Creator'), array('no_escape' => true));
         $entry['link'] = xml_escape(record_url($item, null, true));
         $entry['lastUpdate'] = strtotime($item->added);
         return $entry;
