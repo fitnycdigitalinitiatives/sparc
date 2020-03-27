f5ea1b69658310cccd4e2235bb252df7fae776e3
Joseph Anderson <fitnycdigitalinitiatives@users.noreply.github.com>
2020-03-27 15:50:25 -0400

    test

--- items/show.php
+++ items/show.php
@@ -16,7 +16,7 @@
 			<?php endif; ?>
 		</div>
 		<div class="col-sm-6">
-      <!-- Go to www.addthis.com/dashboard to customize your tools -->
+      <!-- Go to www.addthis.com/dashboard to customize your tools. -->
 			<div class="addthis_sharing_toolbox"></div>
     </div>
 	</div>
