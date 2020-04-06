11cd860254c621159d977db8263d43994ce94e27
Joseph Anderson <fitnycdigitalinitiatives@users.noreply.github.com>
2020-04-06 12:41:30 -0400

    updated to work with exhibits

--- exhibit-builder/exhibits/exhibit_footer.php
+++ exhibit-builder/exhibits/exhibit_footer.php
@@ -22,5 +22,61 @@
       </nav>
       <?php fire_plugin_hook('public_footer', array('view' => $this)); ?>
     </footer>
+    <!-- Javascript -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+    <script src="<?php echo src('typeahead.bundle.min', 'javascripts/lib/typeahead', 'js'); ?>"></script>
+    <?php if (@$bodyclass == 'exhibits show'):  ?>
+
+      <script src="https://cdn.jsdelivr.net/npm/openseadragon@2.4/build/openseadragon/openseadragon.min.js" integrity="sha384-BqvbWCNWGAf21sDh6X5DGseJPJ+iNSRIX/j6rxssCsNw1dbPRaX8TiA9gfy3Jd2F" crossorigin="anonymous"></script>
+      <script src="<?php echo src('seadragon-view', 'javascripts', 'js'); ?>"></script>
+    <?php endif; ?>
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  	<script type='text/javascript'>
+      $(function () {
+    	  $('[data-toggle="tooltip"]').tooltip()
+    	})
+  	</script>
+    <script type='text/javascript'>
+      $(function () {
+    	  $('[data-toggle="popover"]').popover({
+        container: 'body'
+        })
+    	})
+  	</script>
+    <script type="text/javascript">
+    $(document).ready(function(){
+      // constructs the suggestion engine
+      // /solr/omeka/terms?terms.fl=tag&terms.limit=-1&omitHeader=true&indent=true&wt=json&json.nl=arrntv
+      var tags = new Bloodhound({
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('tag'),
+      queryTokenizer: Bloodhound.tokenizers.whitespace,
+      prefetch: '<?php echo src('autocomplete_tags_counts_2020-01-03', 'javascripts/lib/typeahead', 'json'); ?>'
+      });
+
+      $('.form-group #query').typeahead({
+      hint: false,
+      highlight: true,
+      minLength: 1
+      },
+      {
+      name: 'tags',
+      display: 'tag',
+      source: tags,
+      limit: 7,
+      templates: {
+        suggestion: function(data){
+              return '<div><span class="badge tag-count pull-right">' + data.count + '</span>' + data.tag + '</div>';
+        }
+      }
+    }).bind('typeahead:select', function(ev, data) {
+        var search_url = '/solr-search?q=&facet=tag%3A%22' + encodeURIComponent(data.tag) + '%22';
+        window.location.href = search_url;
+      });
+    });
+    </script>
   </body>
   </html>
