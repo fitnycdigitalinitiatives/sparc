3aa544ae12cc4a55e34d6b07624549fe24e770da
Joseph Anderson <fitnycdigitalinitiatives@users.noreply.github.com>
2020-10-28 10:48:57 -0400

    separating exhibit css from layouts

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -5,5 +5,17 @@ echo head(array(
 ?>
 
   <?php exhibit_builder_render_exhibit_page(); ?>
+  <div class="row exhibit-nav">
+    <div class="col-xs-12">
+          <?php if ($prevLink = exhibit_builder_link_to_previous_page('<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span><span class="sr-only">Previous</span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round previous'))): ?>
+            <?php echo $prevLink; ?>
+          <?php else: ?>
+            <?php echo exhibit_builder_link_to_exhibit(null, '<span class="glyphicon glyphicon-menu-left" aria-hidden="true"></span><span class="sr-only">Previous</span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round previous')); ?>
+          <?php endif; ?>
+          <?php if ($nextLink = exhibit_builder_link_to_next_page('<span class="glyphicon glyphicon-menu-right" aria-hidden="true"></span><span class="sr-only">Next</span>', array('type' => 'button', 'class' => 'btn btn-default btn-lg btn-round next'))): ?>
+            <?php echo $nextLink; ?>
+          <?php endif; ?>
+    </div>
+  </div>
 
 <?php echo common('exhibit_footer', array('exhibit' => $exhibit, 'exhibit_page' => $exhibit_page, 'bodyclass' => 'exhibits show'), 'exhibit-builder/exhibits'); ?>
